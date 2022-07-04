c0740db074d96631d8551d30ba85893509c432e8
Julianne Ruz <jaruz@up.edu.ph>
2022-07-04 20:15:13 +0800

    Edited build.gradle files.

--- code/Narcissa/app/src/main/java/com/example/narcissa/MainActivity.java
+++ code/Narcissa/app/src/main/java/com/example/narcissa/MainActivity.java
@@ -1,8 +1,9 @@
 // Github token: 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
 // Expires 8/19/2022
 
-// Specialized thesis token: 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
-// Expires 7/20/2022
+// Specialized thesis token: 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
+// Note: key need repo, gist, add:org, workflow
+// Expires 8/3/2022
 
 //  NOTE: "OpenCV error: Cannot load info library for OpenCV"
 //        is only applicable to special Android configs like builds with CUDA support
